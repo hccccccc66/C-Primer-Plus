2b05b6ecb77f24ba45035262b404ff52cfea696c
Laurie <115625063+hccccccc66@users.noreply.github.com>
2024-01-02 14:46:56 +0800

    Create 6.c

---
+++ 第2章/6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int toes = 10;
+	int toes2 = toes * toes;
+	int toes3 = toes2 * toes;
+
+	printf("toes = %d, toes2 = %d, toes3 = %d\n", toes, toes2, toes3);
+
+	return 0;
+}
